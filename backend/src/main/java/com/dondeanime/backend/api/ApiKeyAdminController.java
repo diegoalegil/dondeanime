628858a1f68ba9e69f481dcaece1668ce9abdcc6
diegoalegil <diegogildam@gmail.com>
2026-05-26 01:19:02 +0100

    Sprint 16: add API key usage stats

--- backend/src/main/java/com/dondeanime/backend/api/ApiKeyAdminController.java
+++ backend/src/main/java/com/dondeanime/backend/api/ApiKeyAdminController.java
@@ -1,6 +1,7 @@
 package com.dondeanime.backend.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +24,10 @@ public class ApiKeyAdminController {
     @ResponseStatus(HttpStatus.CREATED)
     public ApiKeyDto create(@Valid @RequestBody ApiKeyCreateRequest request) {
         return apiKeyService.create(request);
+    }
+
+    @GetMapping("/stats")
+    public ApiKeyStatsDto stats() {
+        return apiKeyService.stats();
     }
 }
